fe4a88f90dbfa50b50791530e52863197883f9c7
imran <sherlockholmesad221@gmail.com>
2024-11-03 21:07:25 +0600

    setup vite tailwindcss daisyui

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="text-7xl text-center text-pink-800 font-semibold">
+      gadget
+      <div>
+        <button className="btn btn-primary">Primary</button>
+        <button className="btn btn-secondary">Secondary</button>
+        <button className="btn btn-accent">Accent</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
