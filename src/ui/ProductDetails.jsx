2116a29a9912537a630bb0632502ab9e67772f3b
imran <sherlockholmesad221@gmail.com>
2024-11-04 19:53:51 +0600

    edit ProductDetails

--- src/ui/ProductDetails.jsx
+++ src/ui/ProductDetails.jsx
@@ -1,15 +1,37 @@
-import { useContext } from "react"
-import { ProductContext } from "../context/ProductsProvider"
+import { useContext } from "react";
+import { ProductContext } from "../context/ProductsProvider";
 
 function ProductDetails() {
-    const {productDetails} = useContext(ProductContext);
-    return (
+  const { productDetails } = useContext(ProductContext);
+  console.log(productDetails);
+  return (
+    <div className="flex gap-10">
+      <div>
+        <img className="w-80" src={productDetails.product_image} alt="" />
+      </div>
+      <div>
+        <h3>{productDetails.product_title}</h3>
+        <p>price : ${productDetails.price}</p>
+        <button className="rounded-xl border border-green-400 px-2">
+          {productDetails.availability === true ? `In Stock` : `Out of stock`}
+        </button>
+        <p>{productDetails.description}</p>
         <div>
-            <div>
-                <img className="w-80" src={productDetails.product_image} alt="" />
-            </div>
+          <p>Specification :</p>
+          <ol className="list-decimal ml-4">
+            {productDetails.specification.map((item) => (
+              <li>{item}</li>
+            ))}
+          </ol>
         </div>
-    )
+        <h4>Rating⭐</h4>
+        <div className="flex gap-10">
+          <button className="border border-green-300 px-2">Add to cart</button>
+          <button className="border border-green-300 px-2">Wishlist</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ProductDetails
+export default ProductDetails;
