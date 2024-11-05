3f68da9a3272dfbd3b98036aedae8fe8fcf3453f
imran <sherlockholmesad221@gmail.com>
2024-11-05 22:27:11 +0600

    edit cart and wishlist amount in the Header

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,9 +4,9 @@ import { ProductContext } from "../context/ProductsProvider";
 
 function Header() {
   const { pathname } = useLocation();
-  const { setSelectCategory,setIsDetailsView} = useContext(ProductContext);
-  
-  function handleLogoClick(){
+  const { setSelectCategory, setIsDetailsView,addToCart,wishlist } = useContext(ProductContext);
+
+  function handleLogoClick() {
     setSelectCategory("All Product");
     setIsDetailsView(false);
   }
@@ -23,7 +23,9 @@ function Header() {
   return (
     <div className="bg-purple-400">
       <nav className="flex justify-between">
-        <NavLink onClick={handleLogoClick} to="/">Gadget Heaven</NavLink>
+        <NavLink onClick={handleLogoClick} to="/">
+          Gadget Heaven
+        </NavLink>
         <div className="flex gap-8">
           <NavLink
             className={pathname === "/home" ? `bg-green-600` : ``}
@@ -52,30 +54,35 @@ function Header() {
         </div>
         <div className="flex gap-4">
           <button>
-            <Link
-              className={pathname === "/cart" ? `bg-green-600` : ``}
-              to="/cart"
-            >
-              Cart
-            </Link>
+            <img src="./assets/cart.png" alt="" />
+            <p className={`${addToCart.length === 0 ? "" : `absolute z-10 -mt-10 text-green-700 font-bold ml-8 rounded-full border border-pink-600 px-[4px]`}`}>
+              {addToCart.length === 0 ? "": addToCart.length}
+            </p>
           </button>
           <button>
-            <Link
-              className={pathname === "/wishlist" ? `bg-green-600` : ``}
-              to="/wishlist"
-            >
-              WishList
-            </Link>
+            <img src="./assets/wishlist.png" alt="" />
+            <p className={`${wishlist.length === 0 ? "" : `absolute z-10 -mt-10 text-green-700 font-bold ml-8 rounded-full border border-pink-600 px-[4px]`}`}>
+              {wishlist.length === 0 ? "": wishlist.length}
+            </p>
           </button>
         </div>
       </nav>
       <div className="flex flex-col justify-center">
-        <h2 className="text-center">Upgrade Your Tech Accessorize with Gadget Heaven Accessories</h2>
-        <p className="text-center">Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!</p>
+        <h2 className="text-center">
+          Upgrade Your Tech Accessorize with Gadget Heaven Accessories
+        </h2>
+        <p className="text-center">
+          Explore the latest gadgets that will take your experience to the next
+          level. From smart devices to the coolest accessories, we have it all!
+        </p>
         <button>Shop Now</button>
       </div>
       <div>
-        <img className="max-w-3xl max-h-96 mx-auto" src="/assets/banner.jpg" alt="banner img" />
+        <img
+          className="mx-auto max-h-96 max-w-3xl"
+          src="/assets/banner.jpg"
+          alt="banner img"
+        />
       </div>
     </div>
   );
