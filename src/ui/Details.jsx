003329346e932169ddf9bf3f947ac4a0c2fb9f0f
imran <sherlockholmesad221@gmail.com>
2024-11-06 21:02:45 +0600

    edit Details UI

--- src/ui/Details.jsx
+++ src/ui/Details.jsx
@@ -2,7 +2,7 @@ import ProductDetails from "./ProductDetails"
 
 function Details() {
     return (
-        <div className="mt-10">
+        <div className="mt-10 mb-28">
             <div  className="bg-Purple">
                 <h3 className="text-2xl text-center pt-6 text-white font-semibold">Product Details</h3>
                 <p className="mt-2 max-w-3xl text-white text-center mx-auto pb-48">Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!</p>
